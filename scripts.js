7edfadcce95c1b7f249e60414f4bea0fd9ca9d1a
NMRSantos <170880035+NMRSantos@users.noreply.github.com>
2026-02-10 22:00:35 +0000

    Fixed playTurn and checkForWin. Added spacing and semicolons

--- scripts.js
+++ scripts.js
@@ -3,19 +3,23 @@ let createPlayer = function (name, marker) {
     name,
     marker,
   };
-}
+};
 
 let gameBoard = (function () {
     let board = [];
-    let initBoard = () =>{
+
+    let initBoard = () => {
         board = ["","","","","","","","",""];
-    }
+    };
+
     initBoard();
     const getBoard = () => board;
     const setMark = (position, marker) => {
         board[position] = marker;
     };
+
     const reset = () => initBoard();
+
     return {
         getBoard,
         setMark,
@@ -29,19 +33,24 @@ let gameController = (function () {
     let player2 = createPlayer("Adam", "O");
     let currentPlayer = player1;
     let gameOver = false;
+
     const winningCombos = [
         [0,1,2], [3,4,5], [6,7,8],
         [0,3,6], [1,4,7], [2,5,8],
-        [0,4,8], [2,4,6]]
-    ;
+        [0,4,8], [2,4,6]
+    ];
     
     const playTurn = (position) => {
         if(gameOver) return;
 
-        gameBoard.setMark(position, currentPlayer.marker)
-        console.log(gameBoard.getBoard())
-        checkForWin(currentPlayer.marker)
-        switchPlayer()
+        let board = gameBoard.getBoard();
+        if(board[position] === "") {
+            gameBoard.setMark(position, currentPlayer.marker)
+            console.log(board)
+            checkForWin(currentPlayer.marker)
+        };
+
+        if(!gameOver) switchPlayer();
     };
 
     const switchPlayer = () => {
@@ -49,23 +58,23 @@ let gameController = (function () {
             currentPlayer = player2
         } else {
             currentPlayer = player1
-        }
+        };
     };
 
-const checkForWin = (marker) => {
-    const board = gameBoard.getBoard();
-    for (let i = 0; i < winningCombos.length; i++) {
-        const [a, b, c] = winningCombos[i];
-        if (board[a] === marker && board[b] === marker && board[c] === marker) {
-            gameOver = true;
-            console.log(`${currentPlayer.name} won!`);
-            return true;
-        }
-    }
-    return false;
-}
+    const checkForWin = (marker) => {
+        const board = gameBoard.getBoard();
+        for (let i = 0; i < winningCombos.length; i++) {
+            const [a, b, c] = winningCombos[i];
+            if (board[a] === marker && board[b] === marker && board[c] === marker) {
+                gameOver = true;
+                console.log(`${currentPlayer.name} won!`);
+                return true;
+            };
+        };
+        return false;
+    };
 
     return {
         playTurn
-    }
+    };
 })();
